643dadf5c589e6c94bfd02d6f7880c91424506f5
geidtiphong <geidtiphong@gmail.com>
2017-11-20 10:45:14 +0700

    start my Geolocation project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Vibration } from '@ionic-native/vibration';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private vibration: Vibration) {
+    // Vibrate the device for a second
+    // Duration is ignored on iOS.
+    this.vibration.vibrate(1000);
 
+    // Vibrate 2 seconds
+    // Pause for 1 second
+    // Vibrate for 2 seconds
+    // Patterns work on Android and Windows only
+    this.vibration.vibrate([2000, 1000, 2000]);
+
+    // Stop any current vibrations immediately
+    // Works on Android and Windows only
+    this.vibration.vibrate(0);
   }
 
 }

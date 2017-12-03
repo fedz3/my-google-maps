724500d3220f524126c6ae5cd5460c608f8b060e
geidtiphong <geidtiphong@gmail.com>
2017-12-03 09:08:03 +0700

    try to use Obserable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -25,9 +25,9 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
     Geolocation,
     Vibration
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,27 +1,44 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AboutPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { HttpClient } from '@angular/common/http';
 
 @IonicPage({
-  "name":"About"
+  "name": "About"
 })
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html',
 })
-export class AboutPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+
+export class AboutPage {
+  private user:myImageData[] = []
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private http: HttpClient) { 
+      
+    }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AboutPage');
+    
+    // this.user.userId = 1
+    // user.id = 1
+    // user.title = 'hello'
+    // user.body = 'hello body'
+
+    this.http.get<myImageData[]>('https://jsonplaceholder.typicode.com/posts')
+    .subscribe(res => {
+      console.log(res)
+      // this.user = res
+    })
+    console.log(this.user)
   }
 
+}
+
+interface myImageData {
+  userId?:number,
+  id?:number,
+  title?:string,
+  body?:string
 }

e47954f674feba2cb56319cc1bfb6f03aac94354
geidtiphong <geidtiphong@gmail.com>
2017-12-04 16:35:08 +0700

    test about

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/retry';
+import 'rxjs/add/operator/take';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
+import { Observable } from 'rxjs/Observable';
+import Rx  from "rxjs/Rx";
 
 @IonicPage({
   "name": "About"
@@ -12,33 +18,38 @@ import { HttpClient } from '@angular/common/http';
 
 
 export class AboutPage {
-  private user:myImageData[] = []
+  private posts: MyPosts[] = []
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-    private http: HttpClient) { 
-      
-    }
+    private http: HttpClient) {
+
+  }
 
   ionViewDidLoad() {
-    
-    // this.user.userId = 1
-    // user.id = 1
-    // user.title = 'hello'
-    // user.body = 'hello body'
+    this.getPosts()
+    // let source = Rx.Observable.create(1, 2, 3, 4, 5).take(3)
+    // let myNumbers = source
+    // console.log(myNumbers)
+  }
 
-    this.http.get<myImageData[]>('https://jsonplaceholder.typicode.com/posts')
-    .subscribe(res => {
-      console.log(res)
-      // this.user = res
-    })
-    console.log(this.user)
+  
+
+  getPosts(): void {
+    this.http.get<MyPosts[]>('https://jsonplaceholder.typicode.com/posts', { observe: 'response' })
+      .retry(3)
+      .subscribe({
+        next: (resp) => this.posts = resp.body,
+        error: (err:HttpErrorResponse) => console.log(err),
+        complete: () => console.log('Completed !')
+      })
   }
 
 }
 
-interface myImageData {
-  userId?:number,
-  id?:number,
-  title?:string,
-  body?:string
+interface MyPosts {
+  userId?: number,
+  id?: number,
+  title?: string,
+  body?: string,
+  pop: string
 }
